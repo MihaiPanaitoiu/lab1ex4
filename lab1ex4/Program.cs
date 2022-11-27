27386f721b25a81cf49dd335b761a544b7b88503
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 18:15:13 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Ex 4
+                Scrieti un program care va afisa semnul unui numar citit de la tastatura
+                • Daca numarul este pozitiv, va afisa “+”
+                • Daca numarul este negativ, va afisa “-”
+                • Daca numarul este 0, va afisa “0”
+             */
+
+            Console.WriteLine("Acest program va afisa semnul unui numar citit de la tastatura");
+
+            Console.WriteLine("Introduceti numarul");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("-");
+            } else if (num > 0)
+            {
+                Console.WriteLine("+");
+            } else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
